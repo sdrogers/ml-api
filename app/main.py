2c4502b74d6646c70daf5e84d7b657ea76b89996
Simon Rogers <simon.d.rogers@gmail.com>
2023-05-07 10:34:19 +0100

    now with model training for arbitrary models from sklearn

--- app/main.py
+++ app/main.py
@@ -1,18 +1,30 @@
+import os
+import joblib
+import importlib
 from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 import logging
-import numpy as np
+import pandas as pd
 
-from joblib import load
+from sklearn.svm import SVC
+from sklearn.metrics import roc_auc_score
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__file__)
 
 class InputFeatures(BaseModel):
-    feature_values: list[float]
+    feature_values: dict
     model_name: str
+
+class TrainingDataset(BaseModel):
+    data: dict
+    targets: list
+    model_save_name: str
+    model_module: str
+    model_class: str
+    model_hyperparameters: dict
 
 app = FastAPI()
 
@@ -26,23 +38,48 @@ def read_item(item_id: int, q: Union[str, None] = None):
 
 @app.post("/models/predict")
 def make_prediction(input_features: InputFeatures):
-    svc = load(f'models/{input_features.model_name}.joblib')
-    n_feat = svc.n_features_in_
+    svc = joblib.load(os.path.join('models', f'{input_features.model_name}.joblib'))
+    feat_names = input_features.feature_values.keys()
     try:
-        feats = np.array(
-            input_features.feature_values
-        ).reshape((1, n_feat))
-    except: #TODO handle exception properly
-        message = f"Incorrect number of input features. Sent: {len(input_features.feature_values)}, required: {n_feat}"
-        logger.debug(message)
-        return {"error": message}
+        n_rows = len(input_features.feature_values[list(feat_names)[0]])
+    except TypeError:
+        # single row passed
+        n_rows = 1
+    feats = pd.DataFrame(input_features.feature_values, index=range(n_rows))
+    # ensure that the column orders match
+    for feat_name in feats.columns:
+        if feat_name not in svc.feature_names_in_:
+            msg = f"{feat_name} provided but not in original features"
+            logger.debug(feat_name)
+            return {'error': msg} 
+    feats = feats.reindex(columns=svc.feature_names_in_)
     probs = svc.predict_proba(feats)
-    logger.debug(probs.shape)
+    
     output = {
         'model_name': input_features.model_name,
         'feature_values': input_features.feature_values,
-        'prediction': {}
+        'predictions': {}
     }
     for i, cl in enumerate(svc.classes_):
-        output['prediction'][int(cl)] = probs[0][i]
+        output['predictions'][int(cl)] = list(probs[:, i])
+    return output
+
+@app.post("/models/train")
+def train_model(training_data: TrainingDataset):
+    n_feat = len(training_data.data)
+    n_rows = len(training_data.targets)
+    logger.debug("Training data has %d features and %d rows", n_feat, n_rows)
+    X = pd.DataFrame(training_data.data)
+    y = training_data.targets
+    logger.debug(y)
+
+    class_module = importlib.import_module(training_data.model_module)
+    classifier_class = getattr(class_module, training_data.model_class)
+    classifier = classifier_class(**training_data.model_hyperparameters)
+    classifier.fit(X, y)
+    train_auc = roc_auc_score(y, classifier.predict_proba(X)[:, 1])
+    output = {
+        'train_auc': train_auc
+    }
+    joblib.dump(classifier, os.path.join("models", f"{training_data.model_save_name}.joblib"))
     return output
--- train_svc.py
+++ train_svc.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from sklearn.svm import SVC
 from joblib import dump
 
@@ -13,13 +14,27 @@ X = np.vstack(
 
 y = np.vstack(
     (
-        np.ones((N_PER_CLASS, 1)),
-        np.zeros((N_PER_CLASS, 1))
+        np.ones((N_PER_CLASS, 1), int),
+        np.zeros((N_PER_CLASS, 1), int)
     )
 ).flatten()
 
+X = pd.DataFrame({'x1': X[:, 0], 'x2': X[:, 1]})
+y = pd.DataFrame({'target': y})
+
+
 svc = SVC(probability=True)
-svc.fit(X, y)
+svc.fit(X, y.values.flatten())
 
 dump(svc, 'models/svc.joblib')
+
+# save data to json for testing training endpoint
+traindata = {}
+traindata['data'] = {
+    'f1': list(X['x1'].values),
+    'f2': list(X['x2'].values)
+}
+traindata['targets'] = list(y.values.flatten())
+
+print(traindata)
 
